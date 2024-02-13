74e971cd4e622affb4258b8c33e863c7813ba52c
alfaizuna <alfaizunaulia@gmail.com>
2024-02-13 22:44:16 +0700

    Initial commit

---
+++ src/GamePanel.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 769 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+    }
+
+
+    @Override
+    public void run() {
+
+    }
+}
